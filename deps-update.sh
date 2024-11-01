f574c9f526ca96544e9d354499e1b4349c334e6c
Adrian Hesketh <adrianhesketh@hushmail.com>
2024-11-01 13:16:55 +0000

    chore: bump to Nix installer version v0.27.1

--- deps-update.sh
+++ deps-update.sh
@@ -1,5 +1,5 @@
 set -eux
-wget https://github.com/DeterminateSystems/nix-installer/releases/download/v0.27.0/nix-installer-aarch64-linux
-wget https://github.com/DeterminateSystems/nix-installer/releases/download/v0.27.0/nix-installer-x86_64-linux
+wget https://github.com/DeterminateSystems/nix-installer/releases/download/v0.27.1/nix-installer-aarch64-linux
+wget https://github.com/DeterminateSystems/nix-installer/releases/download/v0.27.1/nix-installer-x86_64-linux
 sha256sum nix-installer-*-linux > deps.sha256sum
 rm nix-installer-*-linux
