3be1bd2ce6aaf494eefd4e5481bc997147c7f441
Adrian Hesketh <adrianhesketh@hushmail.com>
2025-06-06 12:48:36 +0100

    chore: bump flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
